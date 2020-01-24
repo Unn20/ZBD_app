a8d332f256c8b46ab684294c3012f7bc5f8ba3cd
Piotr <pppiotr98@gmail.com>
2020-01-24 22:18:43 +0100

    Add data base generator. Simplyfy records adding functions

--- src/DataBase.py
+++ src/DataBase.py
@@ -1,6 +1,7 @@
 import pymysql
 from src.Logger import Logger
 import json
+import random
 
 
 class Database:
@@ -32,6 +33,8 @@ class Database:
         self.logger.debug("Creating global cursor.")
         self.cursor = self.connection.cursor()
         self.logger.debug("Global cursor created.")
+
+        self.generateDataBase()
 
     def __del__(self):
         """ Close connection with database """
@@ -81,174 +84,185 @@ class Database:
             result[f"cer{noR + 1}"] = dict()
             for noC, column in enumerate(columns):
                 result[f"cer{noR + 1}"][f"{column[0]}"] = row[noC]
-
         return result
 
+    def addRecord(self, tableName, values):
+        """Create columns_str string to hold columns names ready to put into mysql question"""
+        columns = self.getColumns(tableName)
+        columns_str = "("
+        for c in columns:
+            newColumn = str(c)
+            newColumn = "`" + newColumn[2:-3] + "`"
+            columns_str += newColumn + ", "
+        columns_str = columns_str[:-2] + ")"
 
-    def addOvner(self, nip, companyName='NULL', name='NULL', surname='NULL'):
-        """Add ' at the beggining and at the end of a string"""
-        if companyName != 'NULL':
-            companyName = "'" + companyName + "'"
-        if name != 'NULL':
-            name = "'" + name + "'"
-        if surname != 'NULL':
-            surname = "'" + surname + "'"
+        """Create values_str string to hold columns values ready to put into mysql question"""
+        values_str = "("
+        for v in values:
+            newValue = v
+            if newValue != 'NULL':
+                newValue = "'" + str(v) + "'"
+            values_str += newValue + ", "
+        values_str = values_str[:-2] + ")"
 
         """Create and execute statement"""
-        nip = str(nip)
-        statement = "INSERT INTO `wlasciciele`(`nip`, `nazwa_firmy`, `imie`, `nazwisko`) VALUES ( " + nip + ", " + companyName + ", " + name + ", " + surname + " );"
+        tableName = "`" + tableName + "`"
+        statement = "INSERT INTO " + tableName + columns_str + " VALUES " + values_str + ";"
         self.executeStatement(statement)
         self.connection.commit()
 
-    def addAuthor(self, name, surname, birthday, deathday ='NULL'):
-        """Add ' at the beggining and at the end of a string"""
-        if name != 'NULL':
-            name = "'" + name + "'"
-        if surname != 'NULL':
-            surname = "'" + surname + "'"
-        if birthday != 'NULL':
-            birthday = "'" + birthday + "'"
-        if deathday != 'NULL':
-            deathday = "'" + deathday + "'"
+    def generateDataBase(self):
+        names = ['Piotr', 'Maciej', 'Jan', 'Jakub', 'Karol', 'Joanna', 'Marta', 'Magda', 'Natalia', 'Olga']
+        surnames = ['Krol', 'Nowak', 'Zima', 'Leszczyk', 'Karol', 'Nowaczyk', 'Kowalczyk', 'Wozniak', 'Mazur', 'Krawczyk']
+        dates = ['1997-05-05', '1993-12-25', '1991-08-01', '1988-07-17', '1992-10-11', '1991-01-22', '1987-03-30', '1997-11-28', '1990-09-03', '1980-04-14']
+        libraryNames = ['Czytam_ksiazki', 'Czytanie_jest_fajne', 'Ksiazki_sa_super', 'Biblioteka_dziecieca', 'Biblioteka_szkolna', 'Biblioteka_miejska', 'Warto_czytac', 'Super_Biblioteka', 'Biblioteka_na_rynku', 'Biblioteka_na_Kwiatowej']
+        adresses = ['Kwiatowa_10', 'Rzeczypospolitej_5', 'Dluga_4', 'Krutka_1', 'Poznanska_12', 'Wroclawska_43', 'Zawila_3', 'Czysta_21', 'Powstancow_2', 'Brudna_31']
+        genres = ['fantasy', 'horror', 'klasyka', 'kryminal', 'sensacja', 'thriller', 'literatura_mlodziezowa', 'literatura_obyczajowa', 'romans', 'powiesc_historyczna']
+        operationTypes = ['wyporzyczenie', 'zwrot', 'przedluzenie']
+        comments = ['pierwszy raz od dawna', 'sprawna operacja', 'bez uwag']
+        booksTitles = ['Szeptucha', 'Polska_odwraca_oczy', 'On', 'Zycie_na_pelnej_petardzie', 'Okularnik', 'Najgorszy_czlowiek_na_swiecie', 'Inna_dusza', 'Ksiegi_Jakubowe', 'Gniew', 'Trociny']
+        firmNames = ['Januszpol', 'Toyota', 'Adidas', 'Walkman', 'Red_Bull', 'Junkers', 'Ikea', 'Tesco', 'CCC', 'Bakoma']
+        workersFunctions = ['szef', 'sprzatanie', 'obsluga_bazy', 'kucharz']
 
-        """Create and execute statement"""
-        statement = "INSERT INTO `autorzy`(`imie`, `nazwisko`, `data_urodzenia`, `data_smierci`) VALUES ( " + name + ", " + surname + ", " + birthday + ", " + deathday + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
+        """Generate authors"""
+        for _ in range(10):
+            author_id = 'NULL'
+            name = random.choice(names)
+            surname = random.choice(surnames)
+            birthday = random.choice(dates)
+            deathday = random.choice(['NULL', random.choice(dates)])
+            values = [author_id, name, surname, birthday, deathday]
+            self.addRecord('autorzy', values)
 
-    def addGenre(self, name):
-        """Add ' at the beggining and at the end of a string"""
-        if name != 'NULL':
-            name = "'" + name + "'"
+        """Generate libraries"""
+        for i in range(10):
+            name = libraryNames[i]
+            adres = random.choice(adresses)
+            values = [name, adres]
+            self.addRecord('biblioteki', values)
 
-        """Create and execute statement"""
-        statement = "INSERT INTO `gatunki`(`gatunek`) VALUES ( " + name + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
+        """Generate readers"""
+        for _ in range(10):
+            reader_id = 'NULL'
+            name = random.choice(names)
+            surname = random.choice(surnames)
+            values = [reader_id, name, surname]
+            self.addRecord('czytelnicy', values)
 
-    def addSection(self, name, location):
-        """Add ' at the beggining and at the end of a string"""
-        if name != 'NULL':
-            name = "'" + name + "'"
-        if location != 'NULL':
-            location = "'" + location + "'"
-
-        """Create and execute statement"""
-        statement = "INSERT INTO `dzialy`(`nazwa`, `lokalizacja`) VALUES ( " + name + ", " + location + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addReader(self, name, surname):
-        """Add ' at the beggining and at the end of a string"""
-        if name != 'NULL':
-            name = "'" + name + "'"
-        if surname != 'NULL':
-            surname = "'" + surname + "'"
-
-        """Create and execute statement"""
-        statement = "INSERT INTO `czytelnicy`(`imie`, `nazwisko`) VALUES ( " + name + ", " + surname + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addLibrary(self, name, location):
-        """Add ' at the beggining and at the end of a string"""
-        if name != 'NULL':
-            name = "'" + name + "'"
-        if location != 'NULL':
-            location = "'" + location + "'"
-
-        """Create and execute statement"""
-        statement = "INSERT INTO `biblioteki`(`nazwa`, `lokalizacja`) VALUES ( " + name + ", " + location + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addBookstand(self, number, capacity, booksCount, sectionName):
-        """Add ' at the beggining and at the end of a string"""
-        if sectionName != 'NULL':
-            sectionName = "'" + sectionName + "'"
-
-        """Create and execute statement"""
-        number = str(number)
-        capacity = str(capacity)
-        booksCount = str(booksCount)
-        statement = "INSERT INTO `regaly`(`numer`, `pojemnosc`, `liczba_ksiazek`, `dział_nazwa`) VALUES ( " + number + ", " + capacity + ", " + booksCount + ", " + sectionName + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addBook(self, title, publicationDate, genre):
-        """Add ' at the beggining and at the end of a string"""
-        if title != 'NULL':
-            title = "'" + title + "'"
-        if publicationDate != 'NULL':
-            publicationDate = "'" + publicationDate + "'"
-        if genre != 'NULL':
-            genre = "'" + genre + "'"
-
-        """Create and execute statement"""
-        statement = "INSERT INTO `ksiazki`(`tytul`, `data_opublikowania`, `gatunek`) VALUES ( " + title + ", " + publicationDate + ", " + genre + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addAuthorBook(self, author_id, book_id):
-        """Create and execute statement"""
-        author_id = str(author_id)
-        book_id = str(book_id)
-        statement = "INSERT INTO `autor_ksiazka`(`autorzy_autor_id`, `ksiazki_ksiazka_id`) VALUES ( " + author_id + ", " + book_id + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addSpecimen(self, book_id, bookstandNumber):
-        """Create and execute statement"""
-        book_id = str(book_id)
-        bookstandNumber = str(bookstandNumber)
-        statement = "INSERT INTO `egzemplarze`(`ksiazka_id`, `regal_numer`) VALUES ( " + book_id + ", " + bookstandNumber + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addWorker(self, name, surname, function, boss_id = 'NULL'):
-        """Add ' at the beggining and at the end of a string"""
-        if name != 'NULL':
-            name = "'" + name + "'"
-        if surname != 'NULL':
-            surname = "'" + surname + "'"
-        if function != 'NULL':
-            function = "'" + function + "'"
-
-        """Create and execute statement. Disable FK checking"""
-        boss_id = str(boss_id)
-        statement = "INSERT INTO `pracownicy`(`szef_id`, `imie`, `nazwisko`, `funkcja`) VALUES ( " + boss_id + ", " + name + ", " + surname + ", " + function + " );"
+        """Generate sections"""
+        for i in range(10):
+            name = 'dzial' + str(i)
+            location = 'lokalizacja' + str(i)
+            values = [name, location]
+            self.addRecord('dzialy', values)
+            
+        """Generate genres"""
+        for i in range(10):
+            name = genres[i]
+            values = [name]
+            self.addRecord('gatunki', values)
+        
+        """Generate books"""
+        for i in range(10):
+            book_id = 'NULL'
+            title = booksTitles[i]
+            date = random.choice(dates)
+            genre = random.choice(genres)
+            values = [book_id, title, date, genre]
+            self.addRecord('ksiazki', values)
+        
+        """Generate bookstands"""
+        for i in range(10):
+            number = i
+            capacity = random.randint(100,10000)
+            booksCount = capacity - random.randint(0, capacity)
+            section = random.randint(0,9)
+            section = 'dzial' + str(section)
+            values = [number, capacity, booksCount, section]
+            self.addRecord('regaly', values)
+        
+        """Generate ovners"""
+        tmp = random.randint(100,10000)
+        for i in range(10):
+            nip = tmp + i
+            if random.randint(0,1) == 1:
+                firmName = random.choice(firmNames)
+                name = 'NULL'
+                surname = 'NULL'
+            else:
+                firmName = 'NULL'
+                name = random.choice(names)
+                surname = random.choice(surnames)
+            values = [nip, firmName, name, surname]
+            self.addRecord('wlasciciele', values)
+        
+        """Generate ovner-library"""
+        tmp = self.getRawData('wlasciciele')
+        nips = [record[0] for record in tmp]
+        tmp = self.getRawData('biblioteki')
+        libraries = [record[0] for record in tmp]
+        for i in range(len(libraries)):
+            nip = random.choice(nips)
+            library = libraries[i]
+            values = [nip, library]
+            self.addRecord('wlasciciel_biblioteka', values)
+        
+        """Generate author-book"""
+        tmp = self.getRawData('autorzy')
+        authors_ids = [record[0] for record in tmp]
+        tmp = self.getRawData('ksiazki')
+        books_ids = [record[0] for record in tmp]
+        for i in range(len(books_ids)):
+            author_id = random.choice(authors_ids)
+            book_id = books_ids[i]
+            values = [author_id, book_id]
+            self.addRecord('autor_ksiazka', values)
+        
+        """Generate specimens"""
+        tmp = self.getRawData('ksiazki')
+        books_ids = [record[0] for record in tmp]
+        tmp = self.getRawData('regaly')
+        bookstandsNumbers = [record[0] for record in tmp]
+        for _ in range(10):
+            specimen_id = 'NULL'
+            book_id = random.choice(books_ids)
+            bookstandNumber = random.choice(bookstandsNumbers)
+            values = [specimen_id, book_id, bookstandNumber]
+            self.addRecord('egzemplarze', values)
+        
+        """Generate workers. Disable FK checking"""
         self.executeStatement("SET FOREIGN_KEY_CHECKS = 0;")
-        self.executeStatement(statement)
+        for i in range(10):
+            worker_id = 'NULL'
+            if i != 0:
+                boss_id = random.choice(['NULL', random.randint(0, i - 1)])
+            else:
+                boss_id = 'NULL'
+            name = random.choice(names)
+            surname = random.choice(surnames)
+            function = random.choice(workersFunctions)
+            values = [worker_id, boss_id, name, surname, function]
+            self.addRecord('pracownicy', values)
         self.executeStatement("SET FOREIGN_KEY_CHECKS = 1;")
-        self.connection.commit()
 
-    def addOvnerLibrary(self, ovner_nip, library_name):
-        """Add ' at the beggining and at the end of a string"""
-        if library_name != 'NULL':
-            library_name = "'" + library_name + "'"
-
-        """Create and execute statement"""
-        ovner_nip = str(ovner_nip)
-        statement = "INSERT INTO `wlasciciel_biblioteka`(`wlasciciel_nip`, `biblioteka_nazwa`) VALUES ( " + ovner_nip + ", " + library_name + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
-
-    def addOperation(self, date, library_name, worker_id, reader_id, specimen_id, operationType, returnDelay, comments):
-        """Add ' at the beggining and at the end of a string"""
-        if date != 'NULL':
-            date = "'" + date + "'"
-        if library_name != 'NULL':
-            library_name = "'" + library_name + "'"
-        if operationType != 'NULL':
-            operationType = "'" + operationType + "'"
-        if comments != 'NULL':
-            comments = "'" + comments + "'"
-
-        """Create and execute statement"""
-        worker_id = str(worker_id)
-        reader_id = str(reader_id)
-        specimen_id = str(specimen_id)
-        returnDelay = str(returnDelay)
-        statement = "INSERT INTO `historia_operacji`(`data`, `biblioteka_nazwa`, `pracownik_id`, `czytelnik_id`, `egzemplarz_id`, `rodzaj_operacji`, `opoznienie`, `uwagi`) VALUES ( " + date + ", " + library_name + ", " + worker_id + ", " + reader_id + ", " + specimen_id + ", " + operationType + ", " + returnDelay + ", " + comments + " );"
-        self.executeStatement(statement)
-        self.connection.commit()
+        """Generate operations"""
+        tmp = self.getRawData('biblioteki')
+        libraries = [record[0] for record in tmp]
+        tmp = self.getRawData('pracownicy')
+        workers_ids = [record[0] for record in tmp]
+        tmp = self.getRawData('czytelnicy')
+        readers_ids = [record[0] for record in tmp]
+        tmp = self.getRawData('egzemplarze')
+        specimens_ids = [record[0] for record in tmp]
+        for _ in range(10):
+            operation_id = 'NULL'
+            date = random.choice(dates)
+            library = random.choice(libraries)
+            worker_id = random.choice(workers_ids)
+            reader_id = random.choice(readers_ids)
+            specimen_id = random.choice(specimens_ids)
+            operationType = random.choice(operationTypes)
+            delay = random.choice(['NULL', random.randint(1,20)])
+            comment = random.choice(['NULL', random.choice(comments)])
+            values = [operation_id, date, library, worker_id, reader_id, specimen_id, operationType, delay, comment]
+            self.addRecord('historia_operacji', values)
