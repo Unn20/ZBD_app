5cd443731ca82b53ebf808c2c36404e2aaef9e8e
Unn20 <maciox487@gmail.com>
2020-01-28 14:49:54 +0100

    Still errors

--- src/ProcedureController.py
+++ src/ProcedureController.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from src.Logger import Logger
 from src.DataBase import Database
+from tkinter import messagebox
 
 class ProcedureController:
     def __init__(self, rootWindow, database, backEvent):
@@ -13,7 +14,7 @@ class ProcedureController:
         self.logger.debug("ProcedureController logger has started.")
 
         self.mainLabels = ["Library", "Book title", "Genre", "Publishing date"]
-        self.personalLabels = ["Reader's name: ", "Reader's surname:", "Employee's name: ", "Employee's surname: ", "Comment: "]
+        self.personalLabels = ["Reader's name", "Reader's surname", "Employee's name", "Employee's surname", "(opt)Comment"]
 
         self.procedureWindow = Toplevel(self.rootWindow)
         self.procedureWindow.title("Check a book.")
@@ -50,12 +51,13 @@ class ProcedureController:
                                width=self.procedureWindow.winfo_width() - 40,
                                height=20 * 6
                                )
-        self.entries = list()
+        self.entries = dict()
         for no, lab in enumerate(self.personalLabels):
-            Label(self.restFrame, text=lab, font=("Arial Bold", 12)).grid(row=no, column=0, columnspan=3)
+            l = Label(self.restFrame, text=lab, font=("Arial Bold", 12))
+            l.grid(row=no, column=0, columnspan=3)
             entry = Entry(self.restFrame, width=20)
             entry.grid(row=no, column=3, columnspan=2, padx=5, pady=10)
-            self.entries.append(entry)
+            self.entries[l.cget("text")] = entry
 
         self.restFrame.pack(fill='both', side=BOTTOM)
 
@@ -73,22 +75,50 @@ class ProcedureController:
         self.procedureWindow.event_generate("<<back>>")
 
     def applyProcedure(self):
-        for entry in self.entries:
-            if entry.get() == "":
-                print("wypelnij wszystkie pola")
+        args = dict()
+        for label, entry in self.entries.items():
+            if entry.get() == "" and label != "(opt)Comment":
+                self.logger.error("Not all entries are filled")
+                messagebox.showerror("Not all mandatory entries are filled!",
+                                     "Not all mandatory entries are filled!")
                 return
-            print(entry.get())
+            else:
+                args[label] = entry.get()
+
         for combo in self.comboBoxes:
             if combo.combo.get() == "":
-                print("wypelnij wszystkie pola")
+                self.logger.error("Not all entries are filled")
+                messagebox.showerror("Not all mandatory entries are filled!",
+                                     "Not all mandatory entries are filled!")
                 return
-            print(combo.combo.get())
-        # try:
-        #     # Do a hire procedure
-        #     pass
-        # except:
-        #     # show something on exceptions
-        #     pass
+            else:
+                args[combo.label.cget("text")] = combo.combo.get()
+
+        print(args["Library"])
+        print(args["Employee's name"])
+        print(args["Employee's surname"])
+        print(args["Reader's name"])
+        print(args["Reader's surname"])
+        print(args["Book title"])
+        print(args["Publishing date"])
+        print(args["Genre"])
+        print(args["(opt)Comment"])
+
+        try:
+            self.database.borrowBook(args["Library"], args["Employee's name"], args["Employee's surname"],
+                                     args["Reader's name"], args["Reader's surname"], args["Book title"],
+                                     args["Publishing date"], args["Genre"], args["(opt)Comment"])
+        except Exception as e:
+            errorNo = int(e.__str__().split()[0][1:-1])
+            if errorNo == 1644:
+                self.logger.error(f"{e.__str__().split(',')[1][:-2]}")
+                messagebox.showerror("Error while hiring a book.",
+                                     f"{e.__str__().split(',')[1][:-2]}")
+                return
+            else:
+        # show me dat exception
+                print(f"EXCEPTION = {e}")
+                return
         self.goBack()
 
 
