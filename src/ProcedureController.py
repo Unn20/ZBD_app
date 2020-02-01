48b8b3cbfd6f9aa837b6e20a37e8124278b19fd5
Unn20 <maciox487@gmail.com>
2020-02-01 17:40:24 +0100

    Remastered procedure Controller

--- src/ProcedureController.py
+++ src/ProcedureController.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from src.Logger import Logger
 from tkinter import messagebox
+from tkinter import scrolledtext
 
 class ProcedureController:
     def __init__(self, rootWindow, database, backEvent):
@@ -13,7 +14,8 @@ class ProcedureController:
         self.logger.debug("ProcedureController logger has started.")
 
         self.mainLabels = ["Library", "Book title", "Genre", "Publishing date"]
-        self.personalLabels = ["Reader's name", "Reader's surname", "Employee's name", "Employee's surname", "(opt)Comment"]
+        self.personalLabels = ["Reader's surname", "Reader's name", "Employee's surname", "Employee's name",
+                               "Comment(optional)"]
 
         self.procedureWindow = Toplevel(self.rootWindow)
         self.procedureWindow.title("Check a book.")
@@ -31,6 +33,7 @@ class ProcedureController:
             self.frames.append(Frame(self.mainFrame))
             if len(self.comboBoxes) == 0:
                 self.comboBoxes.append(extendedComboBox(self.database, self.frames[-1], lab))
+                self.comboBoxes[-1].combo.configure(state='readonly')
             else:
                 self.comboBoxes.append(extendedComboBox(self.database, self.frames[-1], lab))
                 self.comboBoxes[-2].setChild(self.comboBoxes[-1])
@@ -52,10 +55,27 @@ class ProcedureController:
                                )
         self.entries = dict()
         for no, lab in enumerate(self.personalLabels):
+            #print(no, lab)
             l = Label(self.restFrame, text=lab, font=("Arial Bold", 12))
             l.grid(row=no, column=0, columnspan=3)
-            entry = Entry(self.restFrame, width=20)
+            if lab == "Comment(optional)":
+                # self.commentEntry = Entry(self.restFrame, width=20)
+                # self.commentEntry.grid(row=no, column=3, columnspan=2)
+                self.commentEntry = scrolledtext.ScrolledText(self.restFrame, width=40, height=10)
+                self.commentEntry.grid(row=no, column=3, columnspan=2)
+                continue
+            if lab == "Reader's surname":
+                val = database.executeStatement("SELECT `nazwisko` FROM `pracownicy`")
+            elif lab == "Reader's name":
+                val = database.executeStatement("SELECT `imie` FROM `pracownicy`")
+            elif lab == "Employee's surname":
+                val = database.executeStatement("SELECT `nazwisko` FROM `pracownicy`")
+            elif lab == "Employee's name":
+                val = database.executeStatement("SELECT `imie` FROM `pracownicy`")
+            entry = ttk.Combobox(self.restFrame, values=([""] + list(val)))
+            entry.configure(state='readonly')
             entry.grid(row=no, column=3, columnspan=2, padx=5, pady=10)
+            entry.bind("<<ComboboxSelected>>", lambda _: self.refreshValues())
             self.entries[l.cget("text")] = entry
 
         self.restFrame.pack(fill='both', side=BOTTOM)
@@ -76,7 +96,7 @@ class ProcedureController:
     def applyProcedure(self):
         args = dict()
         for label, entry in self.entries.items():
-            if entry.get() == "" and label != "(opt)Comment":
+            if entry.get() == "":
                 self.logger.error("Not all entries are filled")
                 messagebox.showerror("Not all mandatory entries are filled!",
                                      "Not all mandatory entries are filled!")
@@ -92,11 +112,12 @@ class ProcedureController:
                 return
             else:
                 args[combo.label.cget("text")] = combo.combo.get()
+        args["Comment(optional)"] = self.commentEntry.get('1.0', END)
 
         try:
             self.database.borrowBook(args["Library"], args["Employee's name"], args["Employee's surname"],
                                      args["Reader's name"], args["Reader's surname"], args["Book title"],
-                                     args["Publishing date"], args["Genre"], args["(opt)Comment"])
+                                     args["Publishing date"], args["Genre"], args["Comment(optional)"])
         except Exception as e:
             errorNo = int(e.__str__().split()[0][1:-1])
             if errorNo == 1644:
@@ -111,6 +132,42 @@ class ProcedureController:
                 return
         self.database.connection.commit()
         self.goBack()
+
+    def refreshValues(self):
+        for lab, en in self.entries.items():
+            if lab == "Reader's surname":
+                if en.get() is not "":
+                    val = self.database.executeStatement(f"SELECT `imie` FROM `czytelnicy` " +
+                                                         f"WHERE `nazwisko` = \"{en.get()}\"")
+                else:
+                    val = self.database.executeStatement(f"SELECT `imie` FROM `czytelnicy`")
+                self.entries["Reader's name"].configure(values=([""] + list(val)))
+                #self.entries[lab].set("")
+            elif lab == "Reader's name":
+                if en.get() is not "":
+                    val = self.database.executeStatement(f"SELECT `nazwisko` FROM `czytelnicy` " +
+                                                         f"WHERE `imie` = \"{en.get()}\"")
+                else:
+                    val = self.database.executeStatement(f"SELECT `nazwisko` FROM `czytelnicy`")
+                self.entries["Reader's surname"].configure(values=([""] + list(val)))
+                # self.entries[lab].set("")
+            elif lab == "Employee's surname":
+                if en.get() is not "":
+                    val = self.database.executeStatement(f"SELECT `imie` FROM `pracownicy` " +
+                                                         f"WHERE `nazwisko` = \"{en.get()}\"")
+                else:
+                    val = self.database.executeStatement(f"SELECT `imie` FROM `pracownicy`")
+                self.entries["Employee's name"].configure(values=([""] + list(val)))
+                # self.entries[lab].set("")
+            elif lab == "Employee's name":
+                if en.get() is not "":
+                    val = self.database.executeStatement(f"SELECT `nazwisko` FROM `pracownicy` " +
+                                                         f"WHERE `imie` = \"{en.get()}\"")
+                else:
+                    val = self.database.executeStatement(f"SELECT `nazwisko` FROM `pracownicy`")
+                self.entries["Employee's surname"].configure(values=([""] + list(val)))
+                # self.entries[lab].set("")
+
 
 
 class extendedComboBox:
